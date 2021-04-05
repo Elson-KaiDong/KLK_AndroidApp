5e91268c7ead9a76578eba5215732c754769c564
Kai Dong <elsonchongkaidong@gmail.com>
2021-04-05 17:17:22 +0800

    updated UI for Rubber Reception,FFb Counting and General Worker 05/04/2021

--- IPMS_KLK/Views/MainMenuOptions/FFBCounting.xaml.cs
+++ IPMS_KLK/Views/MainMenuOptions/FFBCounting.xaml.cs
@@ -16,6 +16,63 @@ namespace IPMS_KLK.Views.MainMenuOptions
         {
             InitializeComponent();
             BindingContext = new ViewModels.FFBCountingContentsViewModels.FFBCountingViewModel();
+
+            #region ***Bunch Counting Animation***
+            bunchCounting_btn.Opacity = 0;
+            bunchCounting_btn.TranslationX = -300;
+            bunchCounting_frame.Opacity = 0;
+            bunchCounting_frame.TranslationX = -300;
+            bunchCounting_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            bunchCounting_btn.FadeTo(1, 1000);
+            bunchCounting_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            bunchCounting_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Loose Fruit Weighing Animation***
+            looseFruit_btn.Opacity = 0;
+            looseFruit_btn.TranslationX = -300;
+            looseFruit_frame.Opacity = 0;
+            looseFruit_frame.TranslationX = -300;
+            looseFruit_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            looseFruit_btn.FadeTo(1, 1000);
+            looseFruit_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            looseFruit_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***FFB Verification Animation***
+            ffbVerification_btn.Opacity = 0;
+            ffbVerification_btn.TranslationX = 300;
+            ffbVerification_frame.Opacity = 0;
+            ffbVerification_frame.TranslationX = 300;
+            ffbVerification_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            ffbVerification_btn.FadeTo(1, 1000);
+            ffbVerification_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            ffbVerification_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Amend Verification Animation***
+            amendVerified_btn.Opacity = 0;
+            amendVerified_btn.TranslationX = 300;
+            amendVerified_frame.Opacity = 0;
+            amendVerified_frame.TranslationX = 300;
+            amendVerified_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            amendVerified_btn.FadeTo(1, 1000);
+            amendVerified_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            amendVerified_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Mandore Attendance Animation***
+            mandoreAttendance_btn.Opacity = 0;
+            mandoreAttendance_btn.TranslationX = 300;
+            mandoreAttendance_frame.Opacity = 0;
+            mandoreAttendance_frame.TranslationX = 300;
+            mandoreAttendance_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            mandoreAttendance_btn.FadeTo(1, 1000);
+            mandoreAttendance_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            mandoreAttendance_frame.FadeTo(1, 1000);
+            #endregion
+
+           
         }
 
     }
--- IPMS_KLK/Views/MainMenuOptions/RubberReception.xaml.cs
+++ IPMS_KLK/Views/MainMenuOptions/RubberReception.xaml.cs
@@ -16,6 +16,72 @@ namespace IPMS_KLK.Views.MainMenuOptions
         {
             InitializeComponent();
             BindingContext = new ViewModels.RubberReceptionViewModel();
+
+            #region ***Latex FC Animation***
+            latex_fc_btn.Opacity = 0;
+            latex_fc_btn.TranslationX = -300;
+            latex_fc_frame.Opacity = 0;
+            latex_fc_frame.TranslationX = -300;
+            latex_fc_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            latex_fc_btn.FadeTo(1, 1000);
+            latex_fc_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            latex_fc_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Latex Weighing Animation***
+            latex_weighing_btn.Opacity = 0;
+            latex_weighing_btn.TranslationX = -300;
+            latex_weighing_frame.Opacity = 0;
+            latex_weighing_frame.TranslationX = -300;
+            latex_weighing_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            latex_weighing_btn.FadeTo(1, 1000);
+            latex_weighing_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            latex_weighing_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***FC Weighing Animation***
+            fc_weighing_btn.Opacity = 0;
+            fc_weighing_btn.TranslationX = 300;
+            fc_weighing_frame.Opacity = 0;
+            fc_weighing_frame.TranslationX = 300;
+            fc_weighing_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            fc_weighing_btn.FadeTo(1, 1000);
+            fc_weighing_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            fc_weighing_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Bucket/Cage Wt Animation***
+            bucket_btn.Opacity = 0;
+            bucket_btn.TranslationX = 300;
+            bucket_frame.Opacity = 0;
+            bucket_frame.TranslationX = 300;
+            bucket_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            bucket_btn.FadeTo(1, 1000);
+            bucket_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            bucket_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Print Total Wt Animation***
+            printTotal_btn.Opacity = 0;
+            printTotal_btn.TranslationX = 300;
+            printTotal_frame.Opacity = 0;
+            printTotal_frame.TranslationX = 300;
+            printTotal_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            printTotal_btn.FadeTo(1, 1000);
+            printTotal_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            printTotal_frame.FadeTo(1, 1000);
+            #endregion
+
+            #region ***Show Total Emp Animation***
+            mandore_btn.Opacity = 0;
+            mandore_btn.TranslationX = 300;
+            mandore_frame.Opacity = 0;
+            mandore_frame.TranslationX = 300;
+            mandore_btn.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            mandore_btn.FadeTo(1, 1000);
+            mandore_frame.TranslateTo(0, 0, 1000, Easing.CubicInOut);
+            mandore_frame.FadeTo(1, 1000);
+            #endregion
         }
     }
 }
